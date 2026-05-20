887ec918bc4cc7df066ed1f2aeb2d5b2e5e36c43
empirecornhole-art <dustincarnold@gmail.com>
2026-05-20 09:26:00 -0400

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,10 +1,7 @@
 export type LeagueData = {
-  lastUpdated?: string;
   seasons: string[];
   players: string[];
-  overall: Record<string, any>[];
-  swap: Record<string, any>[];
-  blind: Record<string, any>[];
+  standings: Record<string, any>[];
+  weekly: Record<string, any>[];
   stats: Record<string, any>[];
-  leaderboard: Record<string, any>[];
 };
