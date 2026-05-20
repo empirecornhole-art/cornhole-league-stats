49af3dcf0b0f91ad4dcd20dd3750db9553cfbb85
empirecornhole-art <dustincarnold@gmail.com>
2026-05-20 12:19:17 -0400

    Update blob.ts

--- lib/blob.ts
+++ lib/blob.ts
@@ -5,7 +5,7 @@ const DATA_KEY = "league-data.json";
 
 export async function saveLeagueData(data: LeagueData) {
   await put(DATA_KEY, JSON.stringify(data), {
-    access: "private",
+    access: "public",
     contentType: "application/json",
     allowOverwrite: true,
   });
