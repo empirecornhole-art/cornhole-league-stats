4900c27457a65869818fb688ff2e2aef04c05625
empirecornhole-art <dustincarnold@gmail.com>
2026-05-20 15:41:50 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,11 @@ import "./globals.css";
 
 export const metadata = {
   title: "League Stats",
-  description: "Cornhole League Stats"
+  description: "Empire Cornhole League Stats",
+  icons: {
+    icon: "/icon.png",
+    apple: "/icon.png",
+  },
 };
 
 export default function RootLayout({
