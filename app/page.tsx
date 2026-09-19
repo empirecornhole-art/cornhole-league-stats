1652dfa441e7f7f5887f5ed3d3c9e06cf1b8e33e
empirecornhole-art <dustincarnold@gmail.com>
2026-09-19 19:47:30 -0400

    Wrap LeagueClient in Suspense for loading state

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import LeagueClient from "../components/LeagueClient";
 
 export default function Home() {
-  return <LeagueClient />;
+  return (
+    <Suspense fallback={<main className="min-h-screen bg-black p-6 text-white">Loading League Stats...</main>}>
+      <LeagueClient />
+    </Suspense>
+  );
 }
